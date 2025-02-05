5a712040e8353cd0fd05f3d9258f73dc6fa2cd6c
Naih2021 <munthaliesnai@gmail.com>
2025-02-04 22:00:21 -0500

    added data source, import and provissioner

---
+++ meta-argument/main.tf
@@ -0,0 +1,90 @@
+/*
+countn = 2
+depends_on =
+for each
+lifecycle
+*/
+
+/*resource "aws_instance" "s1" {
+  count= 2 
+  ami = "ami-045269a1f5c90a6a0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "dev-server${count.index}"
+  }
+}
+resource "aws_iam_user" "us1" {
+  name = "cloudadmin"
+  depends_on = [ aws_instance.s1 ]
+}
+
+output "ip" {
+  value = aws_instance.s1[*].public_ip
+}
+*/
+
+/*variable "typeofinstance" {
+  type =list(string)  
+  default = ["t2.micro", "t3.small"]
+}
+variable "info" {
+    type = map(object({
+        ami = string
+        instance = string
+    }))
+    default = {  
+    dev ={ami: "ami-045269a1f5c90a6a0", instance: "t2.micro"}
+    qa = {ami: "ami-045269a1f5c90a6ahjghdfc", instance: "t2.micro"}
+}
+}
+resource "aws_instance" "s2" {
+  for_each = var.info 
+  ami = each.value.ami
+  instance_type = each.value.instance
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_instance" "s2" {
+  for_each = toset(var.typeofinstance)
+  ami = "ami-045269a1f5c90a6a0"
+  instance_type = each.key
+}
+*/
+
+provider "aws"{
+    region = "us-east-1"
+}
+/*
+variable "region" {
+  description = "value of region"
+  type = string
+  sensitive = true
+  //default = "us-east-1"
+} 
+*/
+
+
+provider "aws"{
+    region = "us-east-1"
+    alias = "us1"
+}
+provider "aws"{
+    region = "us-west-1"
+    alias = "usw1"
+}
+
+resource "aws_instance" "server1" {
+  provider = aws.us1
+  ami = "ami-045269a1f5c90a6a0"
+  instance_type = "t2.micro"
+}
+
+
+resource "aws_instance" "server2" {
+  provider = aws.usw1
+  ami = "ami-045269a1f5c90a6a0"
+  instance_type = "t2.micro"
+}
